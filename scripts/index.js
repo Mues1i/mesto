4c3cca47a4eed62986584e67c3076c345dfe2666
Bo Gir <mues1i@yandex.ru>
2023-07-09 17:10:06 +0700

    Исправление после проверки (2-я итерация)

--- scripts/index.js
+++ scripts/index.js
@@ -4,23 +4,20 @@ const popupCloseButton = popup.querySelector('.popup__close');
 // Находим форму в DOM
 const formElement = popup.querySelector('.popup__form');// Воспользуйтесь методом querySelector()
 // Находим поля формы в DOM
-const nameInput = formElement.querySelector('.popup__input_name');// Воспользуйтесь инструментом .querySelector()
-const jobInput = formElement.querySelector('.popup__input_job');// Воспользуйтесь инструментом .querySelector()
+const nameInput = formElement.querySelector('.popup__input_type_name');// Воспользуйтесь инструментом .querySelector()
+const jobInput = formElement.querySelector('.popup__input_type_job');// Воспользуйтесь инструментом .querySelector()
 const profileName = document.querySelector('.profile__name');
 const profileJob = document.querySelector('.profile__job');
 
 function openPopup() {
-  popup.classList.add('popup_opened');
   nameInput.value = profileName.textContent;
   jobInput.value = profileJob.textContent;
+  popup.classList.add('popup_opened');
 }
 
 function closePopup() {
   popup.classList.remove('popup_opened');
 }
-
-profileEditButton.addEventListener('click', openPopup);
-popupCloseButton.addEventListener('click', closePopup);
 
 // Обработчик «отправки» формы, хотя пока
 // она никуда отправляться не будет
@@ -29,16 +26,15 @@ function handleFormSubmit (evt) {
                                               // Так мы можем определить свою логику отправки.
                                               // О том, как это делать, расскажем позже.
 
-  // Получите значение полей jobInput и nameInput из свойства value
-  console.log(nameInput.value);
-  console.log(jobInput.value);
-
    // Вставьте новые значения с помощью textContent
   profileName.textContent = nameInput.value;
   profileJob.textContent = jobInput.value;
   closePopup();
 }
 
+profileEditButton.addEventListener('click', openPopup);
+popupCloseButton.addEventListener('click', closePopup);
 // Прикрепляем обработчик к форме:
 // он будет следить за событием “submit” - «отправка»
 formElement.addEventListener('submit', handleFormSubmit);
+
